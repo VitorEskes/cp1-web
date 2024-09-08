d712626be172453bc5f851adb102c61e7e403dc2
Gabriel <gabrielmsimoes@icloud.com>
2024-09-07 21:54:53 -0300

    V3 Revisando pagina Sobre

--- src/router/Sobre.jsx
+++ src/router/Sobre.jsx
@@ -8,13 +8,13 @@ const Sobre = () => {
             <div className="sobre-content">
                 <h1 className='hsob'>Sobre Nós</h1>
                 <p>
-                    Bem-vindo ao <span className="highlight1">SynthicaSports</span>, o seu portal para tudo sobre esportes. Nosso objetivo é trazer as últimas notícias, análises e conteúdos sobre o mundo dos esportes, desde as modalidades tradicionais até as competições mais modernas.
+                    Bem-vindo à <span className="highlight1">SynthicaSports</span>, a sua loja definitiva para tudo relacionado a esportes! Aqui, você encontra as melhores ofertas em equipamentos esportivos, roupas e acessórios para todas as modalidades. Seja para aprimorar seu desempenho ou para começar uma nova atividade, temos o que você precisa.
                 </p>
                 <p>
-                    Fundada com a missão de conectar fãs ao redor do mundo, <span className="highlight1">SynthicaSports</span> oferece uma plataforma dinâmica para amantes de esportes explorarem suas paixões e descobrirem novas histórias e eventos. Nossa equipe é formada por entusiastas dedicados que trabalham para trazer a você conteúdo de qualidade e de fácil acesso.
+                    Nossa missão na <span className="highlight1">SynthicaSports</span> é fornecer produtos de alta qualidade que atendem às suas necessidades esportivas. Com uma equipe apaixonada por esportes, garantimos que você receba apenas o melhor em termos de performance e estilo. Explore nossa seleção e descubra produtos que vão elevar o seu jogo.
                 </p>
                 <p>
-                    Navegue pelo nosso site para saber mais sobre nossos <a href="/produtos" className="sobre-link">produtos</a> ou entre em <a href="/contato" className="sobre-link">contato</a> conosco para tirar dúvidas. Estamos sempre à disposição para ajudar!
+                    Navegue pelo nosso site para conferir nossa linha de <a href="/produtos" className="sobre-link">produtos</a> ou entre em <a href="/contato" className="sobre-link">contato</a> conosco para qualquer dúvida ou assistência. Estamos aqui para ajudar você a encontrar exatamente o que precisa!
                 </p>
             </div>
             <div className="sobre-image-container">
